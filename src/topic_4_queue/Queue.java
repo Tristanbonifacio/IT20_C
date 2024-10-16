3bd01cf29a4d2242136ea5169974a4505e0ec72d
Tristanbonifacio <tristanbonifacio5@gmail.com>
2024-10-16 15:46:26 +0800

    Added Size

--- src/topic_4_queue/Queue.java
+++ src/topic_4_queue/Queue.java
@@ -15,31 +15,4 @@ public class Queue {
         return customers.size();
     }
     
-    public void enqueue(Customer customer) {
-        customers.addLast(customer);
-        System.out.println(customer.getName() + " has been added to the queue.");
-    }
-    public void displayQueue() {
-        if (isEmpty()) {
-            System.out.println("The queue is currently empty.");
-        } else {
-            System.out.print("Current queue: ");
-            for (Customer customer : customers) {
-                System.out.print(customer.getName() + " | ");
-            }
-            System.out.println();
-        }
-    }
-    
-    public Customer dequeue() {
-        if (!isEmpty()) {
-            Customer customer = customers.removeFirst();
-            System.out.println(customer.getName() + " has been served.");
-            return customer;
-        } else {
-            System.out.println("The queue is empty.");
-            return null;
-        }
-    }
-    
-}
+   
